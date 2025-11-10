5777e1f9b85c0f8bb7646079e68e39b88ed96067
TonyTran2902 <tranbuu2902@gmail.com>
2025-11-10 20:37:34 +0700

    change to deploy

--- client/src/context/authEvents.ts
+++ client/src/context/authEvents.ts
@@ -5,7 +5,9 @@ const listeners = new Set<(event: AuthEvent) => void>();
 export const authEventBus = {
   subscribe: (listener: (event: AuthEvent) => void) => {
     listeners.add(listener);
-    return () => listeners.delete(listener);
+    return () => {
+      listeners.delete(listener);
+    };
   },
   emit: (event: AuthEvent) => {
     listeners.forEach((listener) => listener(event));
